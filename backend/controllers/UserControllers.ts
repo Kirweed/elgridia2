bfdf7ae35625b1c876ae555520b9fd140e7100ed
Kirweed <michal.plichta612@gmail.com>
2023-02-15 18:20:08 +0100

    auth and tokens - backend architecture

--- backend/controllers/UserControllers.ts
+++ backend/controllers/UserControllers.ts
@@ -1,20 +1,32 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import User from "../models/User";
 
 export const loginUser = async (req: Request, res: Response) => {
-  const user = await User.findOne({ username: req.body.username });
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
   if (!user) {
     return res.status(400).send("Cannot find user");
   }
   try {
-    console.log(user.username);
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      res.status(200).send("Success");
+    if (await bcrypt.compare(password, user.password)) {
+      const accessToken = generateAccessToken(username);
+      const refreshToken = generateRefreshToken(username);
+      await User.updateOne(
+        { username },
+        {
+          $push: { refreshTokens: refreshToken },
+        }
+      );
+      res
+        .status(200)
+        .json({ accessToken: accessToken, refreshToken: refreshToken });
     } else {
       res.status(401).send("Failure");
     }
-  } catch {
+  } catch (e) {
+    console.log(e);
     res.status(500).send();
   }
 };
@@ -36,3 +48,72 @@ export const registerUser = async (req: Request, res: Response) => {
     res.status(500).send();
   }
 };
+
+interface UserableRequest extends Request {
+  user?: any;
+}
+
+export const test = (req: UserableRequest, res: Response) => {
+  res.send("success");
+};
+
+export const getNewAccessToken = async (
+  req: Request<{}, {}, { token: string }>,
+  res: Response
+) => {
+  const { token: refreshToken } = req.body;
+  if (!process.env.REFRESH_TOKEN_SECRET) return res.sendStatus(500);
+  if (!refreshToken) return res.sendStatus(401);
+
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET,
+    async (err, user) => {
+      console.log(user);
+      if (err || !user || typeof user === "string") return res.sendStatus(403);
+      try {
+        const dbUser = await User.findOne({ username: user.username });
+        if (!dbUser?.refreshTokens.includes(refreshToken))
+          return res.sendStatus(403);
+
+        const newAccessToken = generateAccessToken(user.username);
+        res.status(200).json({
+          token: newAccessToken,
+        });
+      } catch {
+        res.sendStatus(500);
+      }
+    }
+  );
+};
+
+export const authenticateToken = (
+  req: UserableRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!process.env.ACCESS_TOKEN_SECRET || !process.env.REFRESH_TOKEN_SECRET)
+    return res.sendStatus(500);
+
+  const authHeader = req.headers["authorization"];
+  const token = authHeader?.split(" ")[1];
+
+  if (!token) return res.status(401).send("unauthorized");
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+
+    req.user = user;
+    next();
+  });
+};
+
+const generateAccessToken = (username: string) =>
+  process.env.ACCESS_TOKEN_SECRET
+    ? jwt.sign({ username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 15 })
+    : null;
+
+const generateRefreshToken = (username: string) =>
+  process.env.REFRESH_TOKEN_SECRET
+    ? jwt.sign({ username }, process.env.REFRESH_TOKEN_SECRET)
+    : null;
--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,9 +1,17 @@
 import express from "express";
-import { loginUser, registerUser } from "../controllers/UserControllers";
+import {
+  authenticateToken,
+  getNewAccessToken,
+  loginUser,
+  registerUser,
+  test,
+} from "../controllers/UserControllers";
 
 const router = express.Router();
 
 router.post("/login", loginUser);
 router.post("/register", registerUser);
+router.post("/token", getNewAccessToken);
+router.get("/test", authenticateToken, test);
 
 export default router;
